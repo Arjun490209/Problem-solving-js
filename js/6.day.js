0423cc1d640df1d9c0f382518630f8d0e57c0051
Arjun490209 <colorfulz099@gmail.com>
2026-06-30 21:48:01 +0530

    6.day

---
+++ js/6.day.js
@@ -0,0 +1,13 @@
+// ! Negative arr
+// let arr = [-100, 200, 300, -800, -777, -900];
+// function negNrr(arr) {
+//   let neg = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] < 0) {
+//       neg.push(arr[i]);
+//     }
+//   }
+//   return neg;
+// }
+
+// console.log(negNrr(arr));
